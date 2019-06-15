10477050a494d4f053c6ba74fff29e69279fb3ff
JanSeliv <janseliw@gmail.com>
2019-06-15 23:01:57 +0200

    Getting started with items and bombs

---
+++ Source/Bomber/Bomb.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bomb.h"
+
+// Sets default values
+ABomb::ABomb()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABomb::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABomb::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
