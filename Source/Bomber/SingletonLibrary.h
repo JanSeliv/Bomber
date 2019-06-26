951b4469f72214351526fe19a24a72b28e9f1776
JanSeliv <janseliw@gmail.com>
2019-06-26 21:15:47 +0200

    SingletonLibrary documantation

--- Source/Bomber/SingletonLibrary.h
+++ Source/Bomber/SingletonLibrary.h
@@ -15,41 +15,76 @@ class BOMBER_API USingletonLibrary final : public UBlueprintFunctionLibrary
 public:
 	USingletonLibrary();
 
+	/** 
+	 * The singleton getter
+	 * @return The singleton, nullptr otherwise
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
 	static USingletonLibrary* const GetSingleton();
 
-	/* Cell funs */
-	// Creates a cell from the vector
+	/** @addtogroup cell_functions
+	 * The custom make node of the FCell struct that used instead of the default MakeStruct node
+	 * @param Actor Finding the closest cell by actor
+	 * @return The found cell
+	 */
 	UFUNCTION(BlueprintPure, Category = "C++", meta = (CompactNodeTitle = "toCell"))
-	static const FORCEINLINE FCell MakeCell(const AActor* actor)
+	static const FORCEINLINE FCell MakeCell(const AActor* Actor)
 	{
-		return FCell(actor);
+		return FCell(Actor);
 	}
 
-	// Bound of floor
+	/** @addtogroup cell_functions
+	 * @return The length of one cell (a floor bound)
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++", meta = (DisplayName = "Get Grid Size"))
 	static const FORCEINLINE float GetFloorLength()
 	{
 		return 200.0;
 	}
 
-	// The length between two cells
+	/** @addtogroup cell_functions
+	 * Calculate the length between two cells
+	 * @param X The first cell
+	 * @param Y The other one cell
+	 * @return The distance between to cells
+	 */
 	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "C++")
 	static const FORCEINLINE float CalculateCellsLength(const FCell& X, const FCell& Y)
 	{
 		return (fabsf((X.Location - Y.Location).Size()) / GetFloorLength());
 	}
 
-	/* GeneratedMap funs*/
+	/* The Level Map getter */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++", meta = (WorldContext = "WorldContextObject"))
 	static class AGeneratedMap* const GetLevelMap(UObject* WorldContextObject);
 
 #if WITH_EDITORONLY_DATA
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPushNongeneratedToMap);
+	/** 
+	 * Owners Map Components binds to updating on the Level Map to this delegate
+	 * The Level Map broadcasts this delegate after own generation
+	 * @see class UMapComponent
+	 * @warning PIE only
+	 */
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "C++")
+	FPushNongeneratedToMap OnActorsUpdatedDelegate;
+
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FUpdateAiWithRenderParam);
+	/**
+	 * Dynamic update visualization(text renders) of the bot's movements in the editor
+	 * Debug AI-bot with bShouldShowRenders param binds to this delegate
+	 * Map components owners broadcast this delegate
+	 * on destroying or OnActorsUpdatedDelegate broadcasting
+	 * @warning PIE only
+	 */
+	UPROPERTY()
 	FUpdateAiWithRenderParam OnRenderAiUpdatedDelegate;
 #endif
 
 private:
-	class AGeneratedMap* levelMap_;
+	/** The reference to the AGeneratedMap actor*/
+	class AGeneratedMap* LevelMap_;
+
+	/** Access to the Level Map to keep an in gaming valid reference */
 	friend class AGeneratedMap;
 };
