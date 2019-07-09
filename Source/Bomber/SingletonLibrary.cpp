c5e4826438055aa51459df45480ee65e3261e560
JanSeliv <janseliw@gmail.com>
2019-07-09 23:49:55 +0200

    Debug visualization  of AI moves by text renders

--- Source/Bomber/SingletonLibrary.cpp
+++ Source/Bomber/SingletonLibrary.cpp
@@ -3,28 +3,73 @@
 #include "SingletonLibrary.h"
 
 #include "Bomber.h"
-#include "Engine.h"
+#include "Components/TextRenderComponent.h"
 #include "GeneratedMap.h"
 #include "Kismet/GameplayStatics.h"
+#include "Math/Color.h"
+#include "MyAiCharacter.h"
 
-USingletonLibrary::USingletonLibrary()
+#if WITH_EDITOR
+void USingletonLibrary::BroadcastAiUpdating(AActor* Owner)
 {
-}
-
-USingletonLibrary* const USingletonLibrary::GetSingleton()
-{
-	if (GEngine == nullptr)
+	if (LevelMap_ == nullptr)  // The Level map is null
 	{
-		return nullptr;
+		return;
 	}
 
-	return Cast<USingletonLibrary>(GEngine->GameSingleton);
+	for (AMyCharacter* const MyCharacterIt : LevelMap_->CharactersOnMap)
+	{
+		AMyAiCharacter* const MyAiCharacter = Cast<AMyAiCharacter>(MyCharacterIt);
+		if (MyAiCharacter != nullptr					   // Successfully cast to AI
+			&& MyAiCharacter->bShouldShowRenders == true)  // Is render AI
+		{
+			MyAiCharacter->UpdateAI();
+		}
+	}
 }
+
+void USingletonLibrary::AddDebugTextRenders_Implementation(
+	AActor* Owner,
+	const TSet<FCell>& Cells,
+	FLinearColor TextColor,
+	float TextHeight,
+	float TextSize,
+	const FText& RenderText,
+	FVector CoordinatePosition,
+	bool bShouldClearChildRenders,
+	TArray<UTextRenderComponent*>& OutTextRenderComponents) const
+{
+	AMyAiCharacter* const MyAiCharacter = Cast<AMyAiCharacter>(Owner);
+	if ((MyAiCharacter != nullptr							// Successfully cast to AI
+			&& MyAiCharacter->bShouldShowRenders == false)  // Is not render AI
+		|| Cells.Num() == NULL								// Null length
+		|| IS_VALID(Owner) == false)						// Owner is not valid
+	{
+		return;
+	}
+
+	if (bShouldClearChildRenders == true)  // Should remove all text renders
+		for (auto& RenderIt : Owner->GetComponentsByClass(UTextRenderComponent::StaticClass()))
+		{
+			RenderIt->DestroyComponent();
+		}
+
+	const int32 ArrayNum = (CoordinatePosition == FVector(0.f) ? Cells.Num() : Cells.Num() * 2);
+	for (int32 i = 0; i < ArrayNum; ++i)
+	{
+		OutTextRenderComponents.Add(NewObject<UTextRenderComponent>(Owner));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("%s's num of renders: %d (%s)"), *RenderText.ToString(), Owner->GetComponentsByClass(UTextRenderComponent::StaticClass()).Num(), *CoordinatePosition.ToString());
+}
+
+#endif
 
 AGeneratedMap* const USingletonLibrary::GetLevelMap(UObject* WorldContextObject)
 {
-	if (GEngine == nullptr			   // Global engine pointer is null
-		|| GetSingleton() == nullptr)  // Singleton is null
+	if (GEngine == nullptr				   // Global engine pointer is null
+		|| GetSingleton() == nullptr	   // Singleton is null
+		|| WorldContextObject == nullptr)  // WorldContext is null
 	{
 		return nullptr;
 	}
@@ -32,17 +77,37 @@ AGeneratedMap* const USingletonLibrary::GetLevelMap(UObject* WorldContextObject)
 // Find editor level map
 #if WITH_EDITOR
 	UWorld* const World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::ReturnNull);
-
-	if (World != nullptr &&								  // World is null
-		World->HasBegunPlay() == false					  // for editor only
-		&& IS_VALID(GetSingleton()->LevelMap_) == false)  // current map is not valid
+	if (IS_PIE(World) == true)  // for editor only
 	{
+		// Debug IS_VALID(Obj) and IS_TRANSIENT(Obj) macros
+		int Counter = 0;
+		AGeneratedMap* Obj = GetSingleton()->LevelMap_;
+		if (IsValid(Obj))
+		{
+			++Counter;  // #1
+			if ((Obj)->IsValidLowLevel())
+			{
+				++Counter;  // #2
+				if (!Obj->HasAllFlags(RF_Transient))
+				{
+					++Counter;  // #3
+					if (UGameplayStatics::GetCurrentLevelName(Obj->GetWorld()) != "Transient")
+					{
+						return GetSingleton()->LevelMap_;
+					}
+				}
+			}
+		}
+
+		UE_LOG(LogTemp, Warning, TEXT("Exit state: %i"), Counter);
+
+		// Find and update the Level Map
 		TArray<AActor*> LevelMapArray;
 		UGameplayStatics::GetAllActorsOfClass(World, AGeneratedMap::StaticClass(), LevelMapArray);
 		if (LevelMapArray.Num() > 0)
 		{
 			GetSingleton()->LevelMap_ = Cast<AGeneratedMap>(LevelMapArray[0]);
-			UE_LOG_STR("SingletonLibrary:GetLevelMap: %s UPDATED", LevelMapArray[0]);
+			UE_LOG_STR("PIE:SingletonLibrary:GetLevelMap: %s UPDATED", LevelMapArray[0]);
 		}
 		else
 		{
--- Source/Bomber/SingletonLibrary.h
+++ Source/Bomber/SingletonLibrary.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "Cell.h"
+#include "Engine.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 
 #include "SingletonLibrary.generated.h"
@@ -13,31 +14,73 @@ class BOMBER_API USingletonLibrary final : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	USingletonLibrary();
+#if WITH_EDITORONLY_DATA
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPushNongeneratedToMap);
+	/** 
+	 * Owners Map Components binds to updating on the Level Map to this delegate
+	 * The Level Map broadcasts this delegate after own generation
+	 * @see class UMapComponent
+	 * @warning PIE only
+	 */
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "C++")
+	FPushNongeneratedToMap OnActorsUpdatedDelegate;
+#endif  // WITH_EDITORONLY_DATA
+
+	/** @addtogroup AI
+	 * Call all signed as bShouldShowRenders AI characters
+	 * @param Owner The called owner
+	 * @warning PIE only
+	 * @warning Is not static for OnDestroyed binding
+	 */
+	UFUNCTION(BlueprintCallable, Category = "C++", meta = (DevelopmentOnly))
+	void BroadcastAiUpdating(AActor* Owner);
+
+	/**
+	 * Debug visualization by text renders
+	 * @warning PIE only
+	 * @warning Is not static for Blueprint realization
+	 * @todo Don't remove nameplate render of AI
+     */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintPure = false, Category = "C++", meta = (DevelopmentOnly, AdvancedDisplay = 2, AutoCreateRefTerm = "RenderText"))
+	void AddDebugTextRenders(
+		class AActor* Owner,										  ///<
+		const TSet<struct FCell>& Cells,							  ///<
+		struct FLinearColor TextColor,								  ///<
+		float TextHeight,											  ///<
+		float TextSize,												  ///<
+		const FText& RenderText,									  ///<
+		FVector CoordinatePosition,									  ///<
+		bool bShouldClearChildRenders,								  ///<
+		TArray<class UTextRenderComponent*>& OutTextRenderComponents  ///<
+		) const;
+
+	/** @addtogroup cell_functions
+	 * The custom make node of the FCell struct that used as a blueprint implementation of the default MakeStruct node
+	 * @param Actor Finding the closest cell by actor
+	 * @return The found cell
+	 * @warning Deprecated, temporary function
+	 * @warning Is not static for Blueprint realization
+	 * @todo Rewrite to C++ FCell()
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, BlueprintPure, Category = "C++", meta = (CompactNodeTitle = "toCell"))
+	FORCEINLINE struct FCell MakeCell(const class AActor* Actor) const;
 
 	/** 
 	 * The singleton getter
 	 * @return The singleton, nullptr otherwise
 	 */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
-	static USingletonLibrary* const GetSingleton();
-
-	/** @addtogroup cell_functions
-	 * The custom make node of the FCell struct that used instead of the default MakeStruct node
-	 * @param Actor Finding the closest cell by actor
-	 * @return The found cell
-	 */
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++", meta = (CompactNodeTitle = "toCell"))
-	static const FORCEINLINE FCell MakeCell(const AActor* Actor)
+	static FORCEINLINE USingletonLibrary* GetSingleton()
 	{
-		return FCell(Actor);
+		if (GEngine == nullptr) return nullptr;
+		return Cast<USingletonLibrary>(GEngine->GameSingleton);
 	}
 
 	/** @addtogroup cell_functions
 	 * @return The length of one cell (a floor bound)
 	 */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++", meta = (DisplayName = "Get Grid Size"))
-	static const FORCEINLINE float GetFloorLength()
+	static FORCEINLINE float GetFloorLength()
 	{
 		return 200.0;
 	}
@@ -49,7 +92,7 @@ public:
 	 * @return The distance between to cells
 	 */
 	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "C++")
-	static const FORCEINLINE float CalculateCellsLength(const FCell& X, const FCell& Y)
+	static FORCEINLINE float CalculateCellsLength(const struct FCell& X, const struct FCell& Y)
 	{
 		return (fabsf((X.Location - Y.Location).Size()) / GetFloorLength());
 	}
@@ -58,31 +101,9 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++", meta = (WorldContext = "WorldContextObject"))
 	static class AGeneratedMap* const GetLevelMap(UObject* WorldContextObject);
 
-#if WITH_EDITORONLY_DATA
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPushNongeneratedToMap);
-	/** 
-	 * Owners Map Components binds to updating on the Level Map to this delegate
-	 * The Level Map broadcasts this delegate after own generation
-	 * @see class UMapComponent
-	 * @warning PIE only
-	 */
-	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "C++")
-	FPushNongeneratedToMap OnActorsUpdatedDelegate;
-
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FUpdateAiWithRenderParam);
-	/**
-	 * Dynamic update visualization(text renders) of the bot's movements in the editor
-	 * Debug AI-bot with bShouldShowRenders param binds to this delegate
-	 * Map components owners broadcast this delegate
-	 * on destroying or OnActorsUpdatedDelegate broadcasting
-	 * @warning PIE only
-	 */
-	UPROPERTY()
-	FUpdateAiWithRenderParam OnRenderAiUpdatedDelegate;
-#endif
-
-private:
+protected:
 	/** The reference to the AGeneratedMap actor*/
+	UPROPERTY(BlueprintReadOnly, Category = "C++")
 	class AGeneratedMap* LevelMap_;
 
 	/** Access to the Level Map to keep an in gaming valid reference */
