3f23f767325089e226f268fc288fbb2c1b6838d5
JanSeliv <janseliw@gmail.com>
2020-10-25 16:08:17 +0100

    Custom shape buttons

---
+++ Source/Bomber/Private/UI/ShapeButton.cpp
@@ -0,0 +1,215 @@
+// Copyright 2020 Yevhenii Selivanov.
+
+#include "UI/ShapeButton.h"
+//---
+#include "UMG.h"
+
+//
+void SShapeButton::SetAdvancedHitTexture(UTexture2D* InTexture)
+{
+	AdvancedHitTexture = InTexture;
+}
+
+//
+void SShapeButton::SetAdvancedHitAlpha(int32 InAlpha)
+{
+	AdvancedHitAlpha = InAlpha;
+}
+
+//
+FReply SShapeButton::OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	if (!NeedExecuteAction(MyGeometry, MouseEvent))
+	{
+		return FReply::Unhandled();
+	}
+	return SButton::OnMouseButtonDown(MyGeometry, MouseEvent);
+}
+
+//
+FReply SShapeButton::OnMouseButtonDoubleClick(const FGeometry& InMyGeometry, const FPointerEvent& InMouseEvent)
+{
+	if (!NeedExecuteAction(InMyGeometry, InMouseEvent))
+	{
+		return FReply::Unhandled();
+	}
+	return SButton::OnMouseButtonDoubleClick(InMyGeometry, InMouseEvent);
+}
+
+//
+FReply SShapeButton::OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	if (!NeedExecuteAction(MyGeometry, MouseEvent))
+	{
+		return FReply::Unhandled();
+	}
+	return SButton::OnMouseButtonUp(MyGeometry, MouseEvent);
+}
+
+//
+FReply SShapeButton::OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	const bool WhatToReturn = NeedExecuteAction(MyGeometry, MouseEvent);
+	if (WhatToReturn != bIsHovered)
+	{
+		bIsHovered = WhatToReturn;
+		if (bIsHovered)
+		{
+			SButton::OnMouseEnter(MyGeometry, MouseEvent);
+		}
+		else
+		{
+			SButton::OnMouseLeave(MouseEvent);
+		}
+	}
+	return SButton::OnMouseMove(MyGeometry, MouseEvent);
+}
+
+//
+void SShapeButton::OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+	if (AdvancedHitTexture)
+	{
+		return;
+	}
+	return SButton::OnMouseEnter(MyGeometry, MouseEvent);
+}
+
+//
+void SShapeButton::OnMouseLeave(const FPointerEvent& MouseEvent)
+{
+	return SButton::OnMouseLeave(MouseEvent);
+}
+
+//
+FCursorReply SShapeButton::OnCursorQuery(const FGeometry& MyGeometry, const FPointerEvent& CursorEvent) const
+{
+	if (!bIsHovered)
+	{
+		return FCursorReply::Unhandled();
+	}
+	TOptional<EMouseCursor::Type> TheCursor = Cursor.Get();
+	return TheCursor.IsSet() ? FCursorReply::Cursor(TheCursor.GetValue()) : FCursorReply::Unhandled();
+}
+
+//
+TSharedPtr<IToolTip> SShapeButton::GetToolTip()
+{
+	return bIsHovered ? SWidget::GetToolTip() : nullptr;
+}
+
+//
+bool SShapeButton::NeedExecuteAction(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) const
+{
+	FTexturePlatformData* const& PlatformData = AdvancedHitTexture ? AdvancedHitTexture->PlatformData : nullptr;
+	if (!PlatformData)
+	{
+		return true;
+	}
+
+	FVector2D LocalPosition = MyGeometry.AbsoluteToLocal(MouseEvent.GetScreenSpacePosition());
+
+	LocalPosition.X = floor(LocalPosition.X);
+	LocalPosition.Y = floor(LocalPosition.Y);
+	LocalPosition /= MyGeometry.GetLocalSize();
+
+	const int32 ImageWidth = PlatformData->SizeX;
+	LocalPosition.X *= ImageWidth;
+	LocalPosition.Y *= PlatformData->SizeY;
+
+	const int32 BufferPosition = (floor(LocalPosition.Y) * ImageWidth) + LocalPosition.X;
+
+	if (!PlatformData->Mips.IsValidIndex(0))
+	{
+		return true;
+	}
+
+	bool bToReturn = true;
+	FTexture2DMipMap& Mip = PlatformData->Mips[0];
+	void* const& ImageData = Mip.BulkData.Lock(LOCK_READ_ONLY);
+	if (!ImageData)
+	{
+		bToReturn = false;
+	}
+	else
+	{
+		FColor* const& ColorData = static_cast<FColor*>(ImageData);
+		if (!ColorData
+			|| ColorData[BufferPosition].A <= AdvancedHitAlpha)
+		{
+			bToReturn = false;
+		}
+	}
+	Mip.BulkData.Unlock();
+
+	return bToReturn;
+}
+
+//
+void UShapeButton::SetAdvancedHitTexture(UTexture2D* InTexture)
+{
+	AdvancedHitTexture = InTexture;
+	if (!MyButton.IsValid())
+	{
+		return;
+	}
+
+	if (const auto& ShapeButton = static_cast<SShapeButton*>(MyButton.Get()))
+	{
+		ShapeButton->SetAdvancedHitTexture(AdvancedHitTexture);
+	}
+}
+
+//
+void UShapeButton::SetAdvancedHitAlpha(int32 InAlpha)
+{
+	AdvancedHitAlpha = InAlpha;
+	if (!MyButton.IsValid())
+	{
+		return;
+	}
+
+	if (const auto& ShapeButton = static_cast<SShapeButton*>(MyButton.Get()))
+	{
+		ShapeButton->SetAdvancedHitAlpha(AdvancedHitAlpha);
+	}
+}
+
+//
+void UShapeButton::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+
+	SetAdvancedHitTexture(AdvancedHitTexture);
+	SetAdvancedHitAlpha(AdvancedHitAlpha);
+}
+
+//
+TSharedRef<SWidget> UShapeButton::RebuildWidget()
+{
+	TSharedPtr<SShapeButton> NewButton = SNew(SShapeButton)
+		.OnClicked(BIND_UOBJECT_DELEGATE(FOnClicked, SlateHandleClicked))
+		.OnPressed(BIND_UOBJECT_DELEGATE(FSimpleDelegate, SlateHandlePressed))
+		.OnReleased(BIND_UOBJECT_DELEGATE(FSimpleDelegate, SlateHandleReleased))
+		.OnHovered_UObject(this, &ThisClass::SlateHandleHovered)
+		.OnUnhovered_UObject(this, &ThisClass::SlateHandleUnhovered)
+		.ButtonStyle(&WidgetStyle)
+		.ClickMethod(ClickMethod)
+		.TouchMethod(TouchMethod)
+		.IsFocusable(IsFocusable);
+
+	NewButton->SetAdvancedHitTexture(AdvancedHitTexture);
+	NewButton->SetAdvancedHitAlpha(AdvancedHitAlpha);
+
+	MyButton = NewButton;
+
+	if (GetChildrenCount())
+	{
+		if (auto ButtonSlot = Cast<UButtonSlot>(GetContentSlot()))
+		{
+			ButtonSlot->BuildSlot(MyButton.ToSharedRef());
+		}
+	}
+
+	return MyButton.ToSharedRef();
+}
---
+++ Source/Bomber/Public/UI/ShapeButton.h
@@ -0,0 +1,76 @@
+// Copyright 2020 Yevhenii Selivanov.
+
+#pragma once
+
+#include "Components/Button.h"
+#include "ShapeButton.generated.h"
+
+/**
+ *
+ */
+class SShapeButton final : public SButton
+{
+public:
+	/** */
+	void SetAdvancedHitTexture(class UTexture2D* InTexture);
+
+	/**  */
+	void SetAdvancedHitAlpha(int32 InAlpha);
+
+protected:
+	/**  */
+	class UTexture2D* AdvancedHitTexture = nullptr;
+
+	/**  */
+	int32 AdvancedHitAlpha = 1;
+
+	virtual FReply OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseButtonDoubleClick(const FGeometry& InMyGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual FReply OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void OnMouseLeave(const FPointerEvent& MouseEvent) override;
+	virtual FCursorReply OnCursorQuery(const FGeometry& MyGeometry, const FPointerEvent& CursorEvent) const override;
+	virtual TSharedPtr<IToolTip> GetToolTip() override;
+
+	/**  */
+	bool NeedExecuteAction(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) const;
+};
+
+
+/**
+ * Unusual shape button
+ * To define a shape, use a texture with an alpha channel
+ */
+UCLASS()
+class BOMBER_API UShapeButton final : public UButton
+{
+	GENERATED_BODY()
+
+public:
+	/** Default constructor. */
+	explicit UShapeButton(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+
+	/**  */
+	UFUNCTION(BlueprintCallable, Category = "C++")
+    void SetAdvancedHitTexture(class UTexture2D* InTexture);
+
+	/** */
+	UFUNCTION(BlueprintCallable, Category = "C++")
+    void SetAdvancedHitAlpha(int32 InAlpha);
+
+protected:
+	/**  */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "C++", meta = (BlueprintProtected))
+	class UTexture2D* AdvancedHitTexture = nullptr;
+
+	/**  */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "C++", meta = (BlueprintProtected, ClampMin = "0.0", ClampMax = "255.0", UIMin = "0.0", UIMax = "255.0"))
+	int32 AdvancedHitAlpha = 1;
+
+	/** */
+	virtual void SynchronizeProperties() override;
+
+	/** */
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+};
