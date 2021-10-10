cd28bd29360734f272ba38afe636989d48a941a2
JanSeliv <janseliw@gmail.com>
2021-10-10 23:09:12 +0200

    Allowing to choose 'function to bind' per each input action

--- Source/Bomber/Public/Globals/MyInputAction.h
+++ Source/Bomber/Public/Globals/MyInputAction.h
@@ -3,10 +3,13 @@
 #pragma once
 
 #include "InputAction.h"
+//---
+#include "Structures/FunctionPicker.h"
+//---
 #include "MyInputAction.generated.h"
 
 /**
-  * Has additional data to setup input action.
+  * Is inherited data asset, has additional data to setup input action.
   */
 UCLASS()
 class BOMBER_API UMyInputAction final : public UInputAction
@@ -14,4 +17,31 @@ class BOMBER_API UMyInputAction final : public UInputAction
 	GENERATED_BODY()
 
 public:
+	/** Returns the chosen state when function has to be called.
+	 * @see UMyInputAction::TriggerEventInternal */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
+	FORCEINLINE ETriggerEvent GetTriggerEvent() const { return TriggerEventInternal; }
+
+	/** Returns the data about static function object getter of a function to bind.
+	 * @see UMyInputAction::StaticContextInternal */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
+	const FORCEINLINE FFunctionPicker& GetStaticContext() const { return StaticContextInternal; }
+
+	/** Returns the function data that is used to be called when input will be triggered.
+	 * @see UMyInputAction::FunctionToBindInternal */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
+	const FORCEINLINE FFunctionPicker& GetFunctionToBind() const { return FunctionToBindInternal; }
+
+protected:
+	/** Choose for which state the bound function has to be called. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Trigger Event", ShowOnlyInnerProperties))
+	ETriggerEvent TriggerEventInternal = ETriggerEvent::Triggered; //[D]
+
+	/** Contains data about static function object getter of a function to bind. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Static Context", ShowOnlyInnerProperties, FunctionContextTemplate))
+	FFunctionPicker StaticContextInternal; //[D]
+
+	/** Allows to set function that is used to be called when input will be triggered. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Function To Bind", ShowOnlyInnerProperties))
+	FFunctionPicker FunctionToBindInternal; //[D]
 };
