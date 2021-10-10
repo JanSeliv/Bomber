d1b28c6a64ef0e9d0fa7d810998cf11480fa448a
JanSeliv <janseliw@gmail.com>
2021-10-10 09:20:45 +0200

    Reparent all input actions to UMyInputAction

---
+++ Source/Bomber/Public/Globals/MyInputAction.h
@@ -0,0 +1,17 @@
+// Copyright 2021 Yevhenii Selivanov
+
+#pragma once
+
+#include "InputAction.h"
+#include "MyInputAction.generated.h"
+
+/**
+  * Has additional data to setup input action.
+  */
+UCLASS()
+class BOMBER_API UMyInputAction final : public UInputAction
+{
+	GENERATED_BODY()
+
+public:
+};
