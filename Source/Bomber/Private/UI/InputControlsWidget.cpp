8751fc70b7b287876a6ec89de181e3bfab78f658
JanSeliv <janseliw@gmail.com>
2021-12-30 13:04:34 +0100

    Styling for the Input Controls widget

--- Source/Bomber/Private/UI/InputControlsWidget.cpp
+++ Source/Bomber/Private/UI/InputControlsWidget.cpp
@@ -1,6 +1,25 @@
 // Copyright 2021 Yevhenii Selivanov
 
 #include "UI/InputControlsWidget.h"
+//---
+#include "Components/InputKeySelector.h"
+#include "Widgets/Input/SInputKeySelector.h"
+
+// Sets the style of the button and its text
+void UInputControlsWidget::SetStyle(UInputKeySelector* InputKeySelector, const FTextBlockStyle& TextStyle, const FButtonStyle& ButtonStyle)
+{
+	SInputKeySelector* SlateInputKeySelector = GetSlateWidget<SInputKeySelector>(InputKeySelector).Get();
+	if (!ensureMsgf(SlateInputKeySelector, TEXT("ASSERT: 'SlateInputKeySelector' is not valid")))
+	{
+		return;
+	}
+
+	SlateInputKeySelector->SetTextStyle(&TextStyle);
+	InputKeySelector->TextStyle = TextStyle;
+
+	SlateInputKeySelector->SetButtonStyle(&ButtonStyle);
+	InputKeySelector->WidgetStyle = ButtonStyle;
+}
 
 // Called after the underlying slate widget is constructed.
 void UInputControlsWidget::NativeConstruct()
