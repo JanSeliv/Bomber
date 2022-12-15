8b8c889737c39eed7d86253b635730ecb9c5616d
JanSeliv <janseliw@gmail.com>
2022-12-15 02:27:45 -0500

    [UE5.1][Bug][Editor] Fixed Level Actors collide with each other on Editor startup, added items actors by default on the level

--- Source/Bomber/Public/PoolManager.h
+++ Source/Bomber/Public/PoolManager.h
@@ -12,9 +12,11 @@
 UENUM(BlueprintType)
 enum class EPoolObjectState : uint8
 {
-	None, ///< Is not handled by Pool Manager
-	Inactive, ///< Contains in pool, is free and ready to be taken
-	Active  ///< Was taken from pool and can be returned back.
+	None,
+	///< Is not handled by Pool Manager
+	Inactive,
+	///< Contains in pool, is free and ready to be taken
+	Active ///< Was taken from pool and can be returned back.
 };
 
 //ENUM_RANGE_BY_FIRST_AND_LAST($ENUM$, $ENUM$::First, $ENUM$::Last);
@@ -108,7 +110,7 @@ public:
 
 	/** Adds specified object as is to the pool by its class to be handled by the Pool Manager. */
 	UFUNCTION(BlueprintCallable, Category = "C++", meta = (DefaultToSelf = "Object"))
-	bool AddToPool(UObject* Object = nullptr);
+	bool AddToPool(UObject* Object = nullptr, EPoolObjectState PoolObjectState = EPoolObjectState::Inactive);
 
 	/** Get the object from a pool by specified class.
 	 *  It creates new object if there no free objects contained in pool or does not exist any.
@@ -144,7 +146,7 @@ public:
 	/** Returns true is specified object is handled by Pool Manager. */
 	UFUNCTION(BlueprintPure, Category = "C++")
 	bool Contains(const UObject* Object) const;
-	
+
 	/** Returns true if specified object is handled by the Pool Manager and was taken from its pool. */
 	UFUNCTION(BlueprintPure, Category = "C++", meta = (DefaultToSelf = "Object"))
 	bool IsActive(const UObject* Object) const;
