e97ea23d209d01d4b05f9c91b8754aaced0d261f
JanSeliv <janseliw@gmail.com>
2024-04-12 21:23:32 -0400

    Revert "Reparented GeneratedMap subsystem type to stay the same in Editor as well as PIE"
    This reverts commit b2aa035bd351b2a2fe10b736ec28990efce4f041.

--- Source/Bomber/Public/Subsystems/GeneratedMapSubsystem.h
+++ Source/Bomber/Public/Subsystems/GeneratedMapSubsystem.h
@@ -2,38 +2,41 @@
 
 #pragma once
 
-#include "Subsystems/EngineSubsystem.h"
+#include "Subsystems/WorldSubsystem.h"
 //---
 #include "GeneratedMapSubsystem.generated.h"
 
 class AGeneratedMap;
 
 /**
- * Provides access to the Generated Map from anywhere in game as well as in editor.
+ * Provides access to the Generated Map and its world from anywhere in game as well as in editor.
  */
 UCLASS()
-class BOMBER_API UGeneratedMapSubsystem final : public UEngineSubsystem
+class BOMBER_API UGeneratedMapSubsystem final : public UWorldSubsystem
 {
 	GENERATED_BODY()
 
 public:
-	/** Returns the Generated Map Subsystem, is checked and will crash if can't be obtained. */
+#pragma region GetGeneratedMapSubsystem
+	/** Returns the Generated Map Subsystem, is checked and wil crash if can't be obtained. */
 	static UGeneratedMapSubsystem& Get();
 
 	/** Returns the pointer to the Generated Map Subsystem. */
-	UFUNCTION(BlueprintPure, Category = "C++")
-	static UGeneratedMapSubsystem* GetGeneratedMapSubsystem();
+	UFUNCTION(BlueprintPure, Category = "C++", meta = (WorldContext = "WorldContextObject"))
+	static UGeneratedMapSubsystem* GetGeneratedMapSubsystem(const UObject* WorldContextObject = nullptr);
+#pragma endregion GetGeneratedMapSubsystem
 
 	/** The Generated Map getter, nullptr otherwise */
 	UFUNCTION(BlueprintPure, Category = "C++", meta = (Keywords = "Level"))
 	AGeneratedMap* GetGeneratedMap() const;
 
 	/** The Generated Map setter. */
-	UFUNCTION(BlueprintCallable, Category = "C++", meta = (Keywords = "Level"))
+	UFUNCTION(BlueprintCallable, Category = "C++")
 	void SetGeneratedMap(AGeneratedMap* InGeneratedMap);
 
 private:
 	/** Is main game actor on persistent level.
 	 * @see UGeneratedMapSubsystem::GetGeneratedMap */
-	TWeakObjectPtr<AGeneratedMap> GeneratedMapInternal = nullptr;
+	UPROPERTY(Transient)
+	TObjectPtr<AGeneratedMap> GeneratedMapInternal = nullptr;
 };
