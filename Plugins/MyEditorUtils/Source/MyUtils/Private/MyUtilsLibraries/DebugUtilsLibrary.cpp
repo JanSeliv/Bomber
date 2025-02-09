96899f3973fc989ac78e9c9545da2bb68591cf8e
JanSeliv <janseliw@gmail.com>
2025-02-08 22:47:53 -0500

    Added Debug Utils Library

---
+++ Plugins/MyEditorUtils/Source/MyUtils/Private/MyUtilsLibraries/DebugUtilsLibrary.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Yevhenii Selivanov
+
+#include "MyUtilsLibraries/DebugUtilsLibrary.h"
+//---
+#if !UE_BUILD_SHIPPING
+#include "HAL/PlatformStackWalk.h"
+#endif // !UE_BUILD_SHIPPING
+
+// Returns previous function in current callstack
+const char* FDebugUtilsLibrary::GetCallerFunctionANSI()
+{
+#if UE_BUILD_SHIPPING
+	static constexpr char UnavailableMsg[] = "None";
+	return UnavailableMsg;
+#else
+	constexpr int32 MaxDepth = 10;
+	constexpr int32 CallerFrameIndex = 3;
+
+	static char FunctionName[1024] = "Unknown caller";
+	uint64 StackTrace[MaxDepth] = {0};
+
+	const int32 Depth = FPlatformStackWalk::CaptureStackBackTrace(StackTrace, MaxDepth);
+	if (Depth <= CallerFrameIndex)
+	{
+		return FunctionName;
+	}
+
+	FProgramCounterSymbolInfo SymbolInfo;
+	FPlatformStackWalk::ProgramCounterToSymbolInfo(StackTrace[CallerFrameIndex], SymbolInfo);
+	if (!SymbolInfo.FunctionName)
+	{
+		return FunctionName;
+	}
+
+	FCStringAnsi::Strncpy(FunctionName, SymbolInfo.FunctionName, sizeof(FunctionName) - 1);
+	FunctionName[sizeof(FunctionName) - 1] = '\0';
+
+	char* BracketPos = FCStringAnsi::Strrchr(FunctionName, '(');
+	if (BracketPos)
+	{
+		*BracketPos = '\0';
+	}
+
+	return FunctionName;
+#endif // !UE_BUILD_SHIPPING
+}
---
+++ Plugins/MyEditorUtils/Source/MyUtils/Public/MyUtilsLibraries/DebugUtilsLibrary.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Yevhenii Selivanov
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/** __CALLER__ displays previous function in callstack, can be displayed in log by %hs. e.g:
+ * UE_LOG(LogTemp, Log, TEXT("%hs"), __CALLER__); */
+#define __CALLER__ FDebugUtilsLibrary::GetCallerFunctionANSI()
+
+/**
+ * Contains debug utilities for non-shipping game.
+ * Is useful for debugging and profiling. 
+ */
+class MYUTILS_API FDebugUtilsLibrary
+{
+public:
+	/** Returns previous function in current callstack.
+	 * Can be passed in log by as %hs as __CALLER__ */
+	static const char* GetCallerFunctionANSI();
+};
