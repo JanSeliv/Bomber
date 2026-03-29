4928e23bff2e85762a6f727469bfbc31bdf6ad13
JanSeliv <janseliw@gmail.com>
2026-03-28 20:25:12 -0400

    [Bug] Fixed broken Player tags customization

--- Source/BomberEditor/Private/BmrPlayerTagCustomization.cpp
+++ Source/BomberEditor/Private/BmrPlayerTagCustomization.cpp
@@ -5,9 +5,9 @@
 // UE
 #include "PropertyEditorModule.h"
 
-/** The name of class to be customized: PlayerTag */
+/** The name of class to be customized: BmrPlayerTag */
 // @TODO JanSeliv 2dUuTjyT use 'FPlayerTag::StaticStruct()->GetFName()' as soon as the editor module starts referencing the runtime module
-const FName FBmrPlayerTagCustomization::PropertyClassName = TEXT("PlayerTag");
+const FName FBmrPlayerTagCustomization::PropertyClassName = TEXT("BmrPlayerTag");
 
 // Makes a new instance of this detail layout class for a specific detail view requesting it
 TSharedRef<IPropertyTypeCustomization> FBmrPlayerTagCustomization::MakeInstance()
