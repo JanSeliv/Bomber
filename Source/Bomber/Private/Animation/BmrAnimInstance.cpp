e2d7e6eb161069d0a8a99b093d50cc1d4700a1cc
JanSeliv <janseliw@gmail.com>
2026-06-07 01:39:00 -0400

    [Bug] Fixed AnimBP's blendspace is leaking: moved cached BS-asset property from BP to code and properly cleanup it

---
+++ Source/Bomber/Private/Animation/BmrAnimInstance.cpp
@@ -0,0 +1,69 @@
+// Copyright (c) Yevhenii Selivanov
+
+#include "Animation/BmrAnimInstance.h"
+
+// Bomber
+#include "Components/BmrSkeletalMeshComponent.h"
+#include "DataRegistries/BmrPlayerRow.h"
+
+// GFPM
+#include "GfpmUtils.h"
+
+// DAL
+#include "DalRegistrySubsystem.h"
+
+// UE
+#include "Animation/BlendSpace1D.h"
+#include "GameFeaturesSubsystem.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(BmrAnimInstance)
+
+// Called once when animation instance is created (new skeletal mesh is set)
+void UBmrAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	const FName RowName = GetOwnerPlayerRowName();
+	UDalRegistrySubsystem::Get().ListenForDataRegistryRow<FBmrPlayerRow>(this, RowName, [this](const FBmrPlayerRow& PlayerRow)
+	{
+		MovementBlendspace = PlayerRow.IdleWalkRunBlendSpace.Get();
+	});
+
+	UGameFeaturesSubsystem& GameFeaturesSubsystem = UGameFeaturesSubsystem::Get();
+	GameFeaturesSubsystem.RemoveObserver(this);
+	GameFeaturesSubsystem.AddObserver(this, UGameFeaturesSubsystem::EObserverPluginStateUpdateMode::FutureOnly);
+}
+
+// Called when animation instance is torn down
+void UBmrAnimInstance::NativeUninitializeAnimation()
+{
+	UGameFeaturesSubsystem::Get().RemoveObserver(this);
+
+	if (UDalRegistrySubsystem* DalRegistry = UDalRegistrySubsystem::GetDalRegistrySubsystem())
+	{
+		DalRegistry->UnbindFromDataRegistryRow(this, GetOwnerPlayerRowName());
+	}
+
+	MovementBlendspace = nullptr;
+
+	Super::NativeUninitializeAnimation();
+}
+
+// Called prior to deactivating game feature plugin, when its content is about to be released
+void UBmrAnimInstance::OnGameFeatureDeactivating(const UGameFeatureData* GameFeatureData, FGameFeatureDeactivatingContext& Context, const FString& PluginURL)
+{
+	checkf(GameFeatureData, TEXT("ERROR: [%i] %hs:\n'GameFeatureData' is null!"), __LINE__, __FUNCTION__);
+
+	// Release cached blend space when it belongs to the deactivating plugin, so unload does not report it as leaked
+	if (UGfpmUtils::IsInGameFeatureModule(MovementBlendspace, GameFeatureData))
+	{
+		MovementBlendspace = nullptr;
+	}
+}
+
+// Returns player Data Registry row name of owning mesh, None when owner is not Bomber mesh
+FName UBmrAnimInstance::GetOwnerPlayerRowName() const
+{
+	const UBmrSkeletalMeshComponent* MeshComponent = Cast<UBmrSkeletalMeshComponent>(GetOwningComponent());
+	return MeshComponent ? MeshComponent->GetMeshData().RowName : NAME_None;
+}
---
+++ Source/Bomber/Public/Animation/BmrAnimInstance.h
@@ -0,0 +1,39 @@
+// Copyright (c) Yevhenii Selivanov
+
+#pragma once
+
+#include "Animation/AnimInstance.h"
+
+// UE
+#include "GameFeatureStateChangeObserver.h"
+
+#include "BmrAnimInstance.generated.h"
+
+/**
+ * Base animation instance for Bomber animation blueprint.
+ */
+UCLASS()
+class BOMBER_API UBmrAnimInstance : public UAnimInstance
+    , public IGameFeatureStateChangeObserver
+{
+	GENERATED_BODY()
+
+public:
+	/** Idle/walk/run movement blend space of current character, read by Anim Graph. Resolved from active player Data Registry row, null when no row available or its source plugin unloaded. */
+	UPROPERTY(BlueprintReadWrite, VisibleInstanceOnly, Transient, Category = "[Bomber]")
+	TObjectPtr<class UBlendSpace1D> MovementBlendspace = nullptr;
+
+protected:
+	/** Called once when animation instance is created (new skeletal mesh is set). */
+	virtual void NativeInitializeAnimation() override;
+
+	/** Called when animation instance is torn down. */
+	virtual void NativeUninitializeAnimation() override;
+
+	/** Called prior to deactivating game feature plugin, when its content is about to be released. */
+	virtual void OnGameFeatureDeactivating(const UGameFeatureData* GameFeatureData, FGameFeatureDeactivatingContext& Context, const FString& PluginURL) override;
+
+	/** Returns player Data Registry row name of owning mesh, None when owner is not Bomber mesh. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "[Bomber]", meta = (BlueprintProtected))
+	FName GetOwnerPlayerRowName() const;
+};
